53011f57b116c3f113c82f538a317f8eb8b05b0d
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2026-05-17 19:52:31 -0300

    ultimas modificacoes

--- src/Components/QuestionBox/QuestionBox.tsx
+++ src/Components/QuestionBox/QuestionBox.tsx
@@ -5,7 +5,7 @@ export const QuestionBox = () => {
 
 
     return(
-        <div className="bg-white lg:w-150 w-full lg:h-160 px-20 h-screen flex flex-col items-center justify-center gap-20 rounded-2xl">
+        <div className="bg-white lg:w-150 w-full lg:h-160 m-15 px-2 h-screen flex flex-col items-center justify-center gap-20 rounded-2xl">
 
             <h1 className="text-4xl font-extrabold text-center pt-10">Jogo da Adivinha 🕹️</h1>
 
